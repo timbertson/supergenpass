6b9c56da1daf9b99542e66d3a191d9c7aa8b5c8b
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-01-05 20:44:35 +1100

    moved firefox guess_url into gnome

--- src/lib/gnome/main.py
+++ src/lib/gnome/main.py
@@ -1,7 +1,7 @@
 import commands, os
 from ..command import require_command
 from .. import keyinfo
-from firefox import guess_url
+import keyring
 
 def save_clipboard(data):
 	"""
@@ -23,6 +23,21 @@ def notify(domain):
 	st, output = getstatusoutput("mumbles-send -m '%s - password generated' -t 'SuperGenPass'" % domain)
 	if st:
 		raise RuntimeError, output
+
+
+def guess_url():
+	require_command('expect', package='expect')
+	fresno = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fresno')
+	if getstatusoutput("firefox -remote 'ping()'")[0] != 0:
+		# firefox is not running
+		return None
+	(status, output) = getstatusoutput('\'%s\' -j content.location.href' % (fresno,))
+	if status != 0:
+		raise OSError("fresno failed with output: %s\n" % (output) + 
+		              "make sure you have installed mozRepl and it is turned on\n" + 
+		              "(http://github.com/bard/mozrepl/wikis/home)")
+	return output
+
 
 def get_password():
 	return _store().get_credentials()[1]
