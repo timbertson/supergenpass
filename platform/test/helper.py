d3d6d8d2d606bd57638c2ae70533f54081731aa4
Tim Cuthbertson <tim@gfxmonk.net>
2013-02-18 21:15:40 +1100

    cleanup & 0.2 release

--- platform/test/helper.py
+++ platform/test/helper.py
@@ -3,5 +3,3 @@ import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..','supergenpass_platform'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..','supergenpass'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-
-import eggloader
